664ffccb899ebb29715a2869aa3cad5b71beab83
gersonborja <gerson.borja2018@gmail.com>
2021-03-18 18:06:14 -0600

    Clase que filtra cadenas de texto Varios metodos remover espacios vacios remover comillas quitar codigo html y encriptar

---
+++ modulos/filtrarString.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once 'conexion.php';
+
+class filtrar extends conexionDb {
+
+    public function quitarComillas($string){
+        $nueva = str_replace("'", "", $string);
+        $nuevaDos = str_replace('"', '', $nueva);
+        return $nuevaDos;
+    }
+
+    public function quitarHtml($string){
+        $resultado = strip_tags($string);
+        return $resultado;
+    }
+    
+    public function quitarEspaciosDemas($string){
+        $resultado = trim($string);
+        return $resultado;
+    }
+
+    public function minusculas($string){
+        $resultado = strtolower($string);
+        return $resultado;
+    }
+    
+    public function encriptarString($string){
+        $opciones = [
+            'cost' => 10
+        ];
+        $resultado = password_hash($string, PASSWORD_BCRYPT, $opciones);
+        return $resultado;
+    }
+    
+}
+?>
